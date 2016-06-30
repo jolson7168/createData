18a21316cf979067431dc9eb06a663a6c7bec39b
Joe Olson <jolson7168@nododos.com>
2016-06-30 12:43:47 -0500

    updated with coverage

--- scripts/createAssignments.sh
+++ scripts/createAssignments.sh
@@ -1,1 +1,1 @@
-python ../src/createAssignments.py -c ../config/createAssignments.conf -s ../config/createAssignments01.json
+python ../src/createAssignments.py -c ../config/createAssignments.conf -s ../config/createCoverage.json
