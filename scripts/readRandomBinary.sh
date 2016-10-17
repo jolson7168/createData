fbc0dc5e3ccdede10f126c59c18eaf1838d4bbf2
Joe Olson <jolson7168@nododos.com>
2016-10-17 07:04:51 -0500

    overhauled

--- scripts/readRandomBinary.sh
+++ scripts/readRandomBinary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../src/readRandomBinary.py -c ../config/readRandomBinary.conf
+python ../src/readRandomBinary.py -c ../config/readRandomBinary.conf -s ../config/readRandomBinary.json
 
--- src/readRandomBinary.py
+++ src/readRandomBinary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import logging
 import sys
 import time
@@ -18,8 +20,8 @@ from random import randint
 from riak import RiakClient
 
 cfg = RawConfigParser()
-PRECISION = 100000.0
-
+PRECISION = 1000.0
+MAX_PAYLOAD_SIZE = 1024
 def currentDayStr():
     return time.strftime("%Y%m%d")
 
@@ -44,28 +46,72 @@ def getCmdLineParser():
 
     parser.add_argument('-c', '--config_file', default='../config/readRandomBinary.conf',
                         help='configuration file name (*.ini format)')
+    parser.add_argument('-s', '--scenario_file', default='../config/readRandomBinary.json',
+                        help='scenario file name')
 
     return parser
 
 
-def toUnixTime(dt, precision):
+def toUnixTime(dt, precision = 1000.0):
     dt1 = datetime.strptime(dt, '%Y-%m-%dT%H:%M:%S.%fZ')
     td = dt1 - datetime.fromtimestamp(0)
     return int(td.total_seconds() * precision)
 
-def fromUnixTime(dt, precision):
+def fromUnixTime(dt, precision = 1000.0):
     return datetime.fromtimestamp(dt/precision).strftime("%Y-%m-%dT%H:%M:%S.%fZ")
 
+def splitIntervals(start, end, interval):
+    retval = []
+    if (end - start) >= interval:
+        now = start
+        while now < end:
+            if (now + interval) > end:
+                retval.append((now, end))
+            else:
+                retval.append((now, now+interval))
+            now = now + interval
+    else:
+        retval.append((start,end))
+    return retval
 
+def executeQuery(client, table, asset, start, end, maxInt, expectedPayloadSize, destination):
+    intervals = []
+    results = []
+    totQueries = 0
+    if (end - start) >= maxInt:
+        queryStart = time.time()
+        intervals = splitIntervals(start, end, maxInt)
+        for interval in intervals:
+            fmt = "select data from {3} where time >= {0} and time < {1} and assetid ='{2}'".format(interval[0], interval[1], asset, table)
+            data_set = client.ts_query(table, fmt)
+            results.append(data_set.rows)
+        queryDuration = round((time.time() - queryStart),3)
+        totQueries = len(intervals)
+    else:
+        fmt = "select data from {3} where time >= {0} and time < {1} and assetid ='{2}'".format(start, end, asset, table)
+        queryStart = time.time()
+        data_set = client.ts_query(table, fmt)
+        queryDuration = round((time.time() - queryStart),3)
+        results.append(data_set.rows)
+        totQueries = 1
+    tot = 0
+    for result in results:
+        for row in result:
+            tot = tot + len(row[0])
+    if tot == expectedPayloadSize:
+        status = 'Pass!'
+    else:
+        status = 'Fail!'
+    print('{0},{1},{2},{3},{4},{5},{6}'.format(status, expectedPayloadSize, tot, fromUnixTime(start),fromUnixTime(end), queryDuration, totQueries), file = destination)
 
 
 def processRecord(id1, start, end, data):
     print('----------------------------------------------------------------')
     print('{0}: {1} - {2} / {3}'.format(id1, start, end, data))
 
+
 def readRiakTS(client, tableName, startTime, endTime, assetID, expected, logger):
     qry = "SELECT data FROM {0} WHERE time >= {1} and time < {2} and assetid = '{3}'".format(tableName, startTime, endTime, assetID)
-    #print(qry)
     try:   
         startQTime = time.time()
         data_set = client.ts_query(tableName, qry)
@@ -78,9 +124,58 @@ def readRiakTS(client, tableName, startTime, endTime, assetID, expected, logger)
             print('Match! T1: {2} T2: {3} rt: {0} tt: {1} e: {4}'.format(riakDuration, totalDuration, fromUnixTime(startTime, PRECISION), fromUnixTime(endTime, PRECISION), expected))
         else:
             print('Mismatch! duration: {0} expected: {1} got: {2}'.format(riakDuration, expected, len(total)))        
-        #logger.info("Record written: {0}, Time: {1}, Key: {2}".format(result, riakduration, dataSet1[0][1]))
     except Exception as e:
         print("Error: {}".format(e))
+
+def getPayload(amount, units):
+    if units == 'bytes':
+        return amount
+    elif units == 'kb':
+        return 1024 * amount
+    elif units == 'mb':
+        return 1024 * 1024 * amount
+
+
+def getSource(params):
+    table = params['table']
+    
+    if params['quantum']['units'] == 'hours':
+        length = 60 * 60
+    elif params['quantum']['units'] == 'days':
+        length = 60 * 60 * 24
+    maxInt = int(params['quantum']['precision'] * params['quantum']['maxPartitions'] * length * params['quantum']['num'] )
+    return table, maxInt, params['quantum']['precision']
+
+def getOffset(val, units, precision):
+    if units == 'minutes':
+        return int(precision * 60 * val)
+    elif units == 'hours':
+        return int(precision * 60 * 60 * val)
+    elif units == 'days':
+        return int(precision * 60 * 60 * 24 * val)
+
+def executeScenario(scenario):
+    client = RiakClient(protocol='pbc',nodes=[{ 'host': scenario["server"]["ip"], 'pb_port': scenario["server"]["port"] }])
+    table, maxInterval, precision = getSource(scenario["dataSource"])
+    asset = scenario["id"]
+    destinationLoc = '{0}{1}{2}'.format('/tmp/',scenario["name"],'.csv')
+    destination = open(destinationLoc,'w')
+    if isinstance(scenario["interval"], dict):
+        startTimeInt = toUnixTime(scenario["interval"]["startTime"], precision)
+        endTimeInt = toUnixTime(scenario["interval"]["endTime"], precision)
+        now = startTimeInt
+        offset = getOffset(scenario["interval"]["value"],scenario["interval"]["units"], precision)
+        expectedPayloadSize = getPayload(scenario["interval"]["expected"]["value"], scenario["interval"]["expected"]["units"])
+        while now < endTimeInt:
+            now = now + offset
+            executeQuery(client, table, asset, now, now + offset, maxInterval, expectedPayloadSize, destination)
+    elif isinstance(scenario["interval"], list):
+        for interval in scenario["interval"]:
+            expectedPayload = interval[2]
+            expectedPayloadSize = getPayload(expectedPayload['value'], expectedPayload['units'])
+            executeQuery(client, table, asset, toUnixTime(interval[0], precision), toUnixTime(interval[1], precision), maxInterval, expectedPayloadSize, destination)
+    destination.close()
+    
 
 
 def main(argv):
@@ -96,23 +191,11 @@ def main(argv):
     logger = initLog(rightNow)
     logger.info('Starting Run: '+time.strftime("%Y%m%d%H%M%S")+'  =========================')
 
-    # Get Riak going    
-    client = RiakClient(protocol='pbc',nodes=[{ 'host': cfg.get('riakts', 'ip'), 'pb_port': int(cfg.get('riakts', 'port')) }])
-    table = cfg.get('riakts','table')
-    startDate = toUnixTime('2016-01-01T00:00:00.00000Z',PRECISION)
-    endDate = toUnixTime('2016-12-31T23:59:59.99999Z',PRECISION)
+    # Load the scenarios
+    scenarios = json.loads(open(args.scenario_file).read())
 
-
-    blockSize = 1024        # kb
-    intervalSize = 450      # seconds
-    numIntervals = 24*60*60 / intervalSize    # a day
-    interval = int(intervalSize * PRECISION * numIntervals)
-    expected = int((blockSize*1024)*numIntervals)
-    now = startDate
-    while now < endDate:
-        #readRiakTS(client, table, fromUnixTime(now,PRECISION), fromUnixTime(now+interval,PRECISION), 'asset01', expected, logger)
-        readRiakTS(client, table, now, now+interval, 'asset01', expected, logger)
-        now = now + interval
+    for scenario in scenarios['scenarios']:
+        executeScenario(scenario)
 
     # Clean up
     logger.info('Done! '+time.strftime("%Y%m%d%H%M%S")+'  ==========================')
